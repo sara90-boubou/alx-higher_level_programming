736d72016919882d13ad065ff3382ba18c9698ea
sara90-boubou <saraboubou4@gmail.com>
2024-01-09 16:04:17 -0800

    Write a function that writes a string to a text file (UTF8) and returns the number of characters written: > >

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-    cont = 0
-    with open(filename, 'r') as f:
-        for li in f:
-            cont += 1
-    return cont
+"""Defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
