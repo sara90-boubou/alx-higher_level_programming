74ae3b64aa2cb993b398c1ed80cf88891df4cd25
sara90-boubou <saraboubou4@gmail.com>
2024-03-28 17:14:49 -0700

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
+""" Get status in https://intranet.hbtn.io/status"""
 import urllib.request
 
-
-def main():
-    """
-    Funtion to print a response of a specific url
-    """
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf8')))
-
 if __name__ == "__main__":
-    main()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}".format(type(html), html))
+        print("\t- utf8 content: {}".format(html.decode('UTF-8')))
