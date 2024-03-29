4258e65c50918b4849e76854db921b4355dd8a08
sara90-boubou <saraboubou4@gmail.com>
2024-03-28 17:22:44 -0700

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" Get status in https://intranet.hbtn.io/status"""
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}\n\t- content: {}".format(type(html), html))
-        print("\t- utf8 content: {}".format(html.decode('UTF-8')))
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read().decode('utf-8')
+
+print("\t- Body response:")
+print("\t\t- type:", type(body))
+print("\t\t- content:", body)
+
